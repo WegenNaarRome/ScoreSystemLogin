d951b22ee721670d889e4ff33c240737f1f8d843
roycroezen <roy.croezen2015@gmail.com>
2024-03-13 11:12:11 +0100

    css

--- admin.php
+++ admin.php
@@ -7,11 +7,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/root.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
-        <h1>Administrator</h1>
+        <h1>Over het mbo</h1>
     </header>
     <main>
         <a href="admin-pages/spellen.php">Spellen beheren</a>
